b61475e8455b6bf6502033ba8da8f7582d90e454
btc <btc121314@gmail.com>
2020-10-25 23:19:45 +0100

    started log functions

--- XBMath.h
+++ XBMath.h
@@ -1,10 +1,9 @@
 #ifndef XBMATH_H
 #define XBMATH_H
-
 //--------------------------------------------------------constants-----------------------------------------------------------------//
 
 #define PI = 3.14159265358979323846264338327950288419716939937510
-#define e =  2.71828182845904523536028747135266249775724709369995
+#define e = 2.71828182845904523536028747135266249775724709369995
 
 //------------------------------------------------------abs function----------------------------------------------------------------//
 
@@ -32,6 +31,7 @@ int abs(int number)
 		return number;
 }
 */
+
 //---------------------------------------------------------power function-----------------------------------------------------------//
 
 double pow(double base, double power)
@@ -48,7 +48,8 @@ double pow(double base, double power)
 	}
 	else if(power < 1 && power > 0)
 	{
-	//TODO calculate a^(x<1) without root func	
+	//TODO calculate a^(x<1) without root func
+		
 	}
 	else if(power < 0)
 	{
@@ -57,7 +58,8 @@ double pow(double base, double power)
 
 	return result;
 }
-/*	it seems to be useless because if int k = pow(int, int) there will be convertion 
+
+/*	it seems to be useless because if int k = pow(int, int) there will be convertion  anyway
 float pow(float base, float power)
 {
 	float result = 1;
@@ -91,5 +93,41 @@ int pow(int base, int power)
 	return result;
 }
 */
+
+//----------------------------------------------------------------logarythm functions----------------------------------------------//
+double log(double argument, double base)
+{
+	double result = 1;   //agbitrary number, could be anything
+	bool goodEnough = false;
+	int counter = 0;      //after some number of iterations it's close enough so we can stop and give output
+	while(!goodEnough)
+	{
+		double check = pow(base , result);
+		if(check == argument)
+			goodEnough = true;
+
+		else if(check < argument)
+			if(argument - check < 1)
+				result += result/10;
+			else if(argument - check > 1)
+				result += 1;
+
+		else if(check > argument)
+			if(check - argument < 1)
+				result -= result/10;
+			else if(check - argument > 1)
+				result -= 1;
+
+		if(counter >= 10 * argument)  //not sure about this if statement
+			goodEnough = true;
+		counter++;
+	}
+	return result;
+}
+
+double ln(double argument)
+{
+	return log(argument, e);
+}
 
 #endif
